79c6f8ebed04fe9f41d88e459294348d6c9a180c
MrTj458 <mrtj458@gmail.com>
2018-10-27 12:52:18 -0600

    Basics done

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -11,7 +11,9 @@ class Calculator extends React.Component {
 	state = { display: '' }
 
 	addNum = (num) => {
-		this.setState( state => ({ display: state.display + num }) )
+		if(this.state.display.length < 25) {
+			this.setState( state => ({ display: state.display + num }) )
+		}
 	}
 
 	clear = () => {
@@ -23,237 +25,255 @@ class Calculator extends React.Component {
 	}
 
 	calculate = () => {
+		let equ = this.state.display.toString()
+		equ = equ.replace(/\^/g, '**').replace(/x+/g, '*')
+		console.log(equ)
 
+		try {
+			const answer = eval(equ)
+			this.setState({ display: answer.toString() })
+		} catch {
+			this.setState({ display: 'Error' })
+		}
 	}
 
 	render() {
 		const { display } = this.state
 		return (
-			<Container>
+			<Container style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
 				<Header as="h1" textAlign="center">Calculator</Header>
-				<Grid celled centered>
-					{/* ROW 1 */}
-					<Grid.Row>
-						<Grid.Column width={8}>
-							<Segment>
-								<Header as="h2">{display}</Header>
-							</Segment>
-						</Grid.Column>
-					</Grid.Row>
+					<Grid style={{ width: '30vw', backgroundColor: 'darkgray'}} celled centered>
+						{/* ROW 1 */}
+						<Grid.Row>
+							<Grid.Column width={16}>
+								<Segment>
+									<Header style={{ height: '25px' }} as="h2">{display}</Header>
+								</Segment>
+							</Grid.Column>
+						</Grid.Row>
 
-					{/* ROW 2 */}
-					<Grid.Row>
-						<Grid.Column width={2}>
-							<Button
-								name="C"
-								onClick={this.clear}
-								fluid
-							>
-								{'C'}
-							</Button>
-						</Grid.Column>
-						<Grid.Column width={2}>
-							<Button
-								name="<"
-								onClick={this.back}
-								fluid
-							>
-								{"<"}
-							</Button>
-						</Grid.Column>
-						<Grid.Column width={2}>
-							<Button
-								name="("
-								onClick={() => this.addNum('(')}
-								fluid
-							>
-								{'('}
-							</Button>
-						</Grid.Column>
-						<Grid.Column width={2}>
-							<Button
-								name=")"
-								onClick={() => this.addNum(')')}
-								fluid
-							>
-								{')'}
-							</Button>
-						</Grid.Column>
-					</Grid.Row>
+						{/* ROW 2 */}
+						<Grid.Row>
+							<Grid.Column width={4}>
+								<Button
+									name="C"
+									onClick={this.clear}
+									fluid
+								>
+									{'C'}
+								</Button>
+							</Grid.Column>
+							<Grid.Column width={4}>
+								<Button
+									name="<"
+									onClick={this.back}
+									fluid
+								>
+									{"<"}
+								</Button>
+							</Grid.Column>
+							<Grid.Column width={4}>
+								<Button
+									name="("
+									onClick={() => this.addNum('(')}
+									fluid
+								>
+									{'('}
+								</Button>
+							</Grid.Column>
+							<Grid.Column width={4}>
+								<Button
+									name=")"
+									onClick={() => this.addNum(')')}
+									fluid
+								>
+									{')'}
+								</Button>
+							</Grid.Column>
+						</Grid.Row>
 
-					{/* ROW 3 */}
-					<Grid.Row>
-						<Grid.Column width={4}>
-							<Button
-								name="^"
-								onClick={() => this.addNum('^')}
-								fluid
-							>
-								{'^'}
-							</Button>
-						</Grid.Column>
-						<Grid.Column width={4}>
-							<Button
-								name="%"
-								onClick={() => this.addNum('%')}
-								fluid
-							>
-								{'%'}
-							</Button>
-						</Grid.Column>
-					</Grid.Row>
+						{/* ROW 3 */}
+						<Grid.Row>
+							<Grid.Column width={8}>
+								<Button
+									name="^"
+									onClick={() => this.addNum('^')}
+									fluid
+								>
+									{'^'}
+								</Button>
+							</Grid.Column>
+							<Grid.Column width={4}>
+								<Button
+									name="%"
+									onClick={() => this.addNum('%')}
+									fluid
+								>
+									{'%'}
+								</Button>
+							</Grid.Column>
+							<Grid.Column width={4}>
+								<Button
+									name="/"
+									onClick={() => this.addNum('/')}
+									fluid
+								>
+									{'/'}
+								</Button>
+							</Grid.Column>
+						</Grid.Row>
 
-					{/* ROW 4 */}
-					<Grid.Row>
-						<Grid.Column width={2}>
-							<Button
-								name="7"
-								onClick={() => this.addNum('7')}
-								fluid
-							>
-								{'7'}
-							</Button>
-						</Grid.Column>
-						<Grid.Column width={2}>
-							<Button
-								name="8"
-								onClick={() => this.addNum('8')}
-								fluid
-							>
-								{'8'}
-							</Button>
-						</Grid.Column>
-						<Grid.Column width={2}>
-							<Button
-								name="9"
-								onClick={() => this.addNum('9')}
-								fluid
-							>
-								{'9'}
-							</Button>
-						</Grid.Column>
-						<Grid.Column width={2}>
-							<Button
-								name="*"
-								onClick={() => this.addNum('x')}
-								fluid
-							>
-								x
-							</Button>
-						</Grid.Column>
-					</Grid.Row>
+						{/* ROW 4 */}
+						<Grid.Row>
+							<Grid.Column width={4}>
+								<Button
+									name="7"
+									onClick={() => this.addNum('7')}
+									fluid
+								>
+									{'7'}
+								</Button>
+							</Grid.Column>
+							<Grid.Column width={4}>
+								<Button
+									name="8"
+									onClick={() => this.addNum('8')}
+									fluid
+								>
+									{'8'}
+								</Button>
+							</Grid.Column>
+							<Grid.Column width={4}>
+								<Button
+									name="9"
+									onClick={() => this.addNum('9')}
+									fluid
+								>
+									{'9'}
+								</Button>
+							</Grid.Column>
+							<Grid.Column width={4}>
+								<Button
+									name="*"
+									onClick={() => this.addNum('x')}
+									fluid
+								>
+									x
+								</Button>
+							</Grid.Column>
+						</Grid.Row>
 
-					{/* ROW 5 */}
-					<Grid.Row>
-						<Grid.Column width={2}>
-							<Button
-								name="4"
-								onClick={() => this.addNum('4')}
-								fluid
-							>
-								{'4'}
-							</Button>
-						</Grid.Column>
-						<Grid.Column width={2}>
-							<Button
-								name="5"
-								onClick={() => this.addNum('5')}
-								fluid
-							>
-								{'5'}
-							</Button>
-						</Grid.Column>
-						<Grid.Column width={2}>
-							<Button
-								name="5"
-								onClick={() => this.addNum('6')}
-								fluid
-							>
-								{'6'}
-							</Button>
-						</Grid.Column>
-						<Grid.Column width={2}>
-							<Button
-								name="-"
-								onClick={() => this.addNum('-')}
-								fluid
-							>
-								{'-'}
-							</Button>
-						</Grid.Column>
-					</Grid.Row>
+						{/* ROW 5 */}
+						<Grid.Row>
+							<Grid.Column width={4}>
+								<Button
+									name="4"
+									onClick={() => this.addNum('4')}
+									fluid
+								>
+									{'4'}
+								</Button>
+							</Grid.Column>
+							<Grid.Column width={4}>
+								<Button
+									name="5"
+									onClick={() => this.addNum('5')}
+									fluid
+								>
+									{'5'}
+								</Button>
+							</Grid.Column>
+							<Grid.Column width={4}>
+								<Button
+									name="5"
+									onClick={() => this.addNum('6')}
+									fluid
+								>
+									{'6'}
+								</Button>
+							</Grid.Column>
+							<Grid.Column width={4}>
+								<Button
+									name="-"
+									onClick={() => this.addNum('-')}
+									fluid
+								>
+									{'-'}
+								</Button>
+							</Grid.Column>
+						</Grid.Row>
 
-					{/* ROW 6 */}
-					<Grid.Row>
-						<Grid.Column width={2}>
-							<Button
-								name="1"
-								onClick={() => this.addNum('1')}
-								fluid
-							>
-								{'1'}
-							</Button>
-						</Grid.Column>
-						<Grid.Column width={2}>
-							<Button
-								name="2"
-								onClick={() => this.addNum('2')}
-								fluid
-							>
-								{'2'}
-							</Button>
-						</Grid.Column>
-						<Grid.Column width={2}>
-							<Button
-								name="3"
-								onClick={() => this.addNum('3')}
-								fluid
-							>
-								{'3'}
-							</Button>
-						</Grid.Column>
-						<Grid.Column width={2}>
-							<Button
-								name="+"
-								onClick={() => this.addNum('+')}
-								fluid
-							>
-								{'+'}
-							</Button>
-						</Grid.Column>
-					</Grid.Row>
+						{/* ROW 6 */}
+						<Grid.Row>
+							<Grid.Column width={4}>
+								<Button
+									name="1"
+									onClick={() => this.addNum('1')}
+									fluid
+								>
+									{'1'}
+								</Button>
+							</Grid.Column>
+							<Grid.Column width={4}>
+								<Button
+									name="2"
+									onClick={() => this.addNum('2')}
+									fluid
+								>
+									{'2'}
+								</Button>
+							</Grid.Column>
+							<Grid.Column width={4}>
+								<Button
+									name="3"
+									onClick={() => this.addNum('3')}
+									fluid
+								>
+									{'3'}
+								</Button>
+							</Grid.Column>
+							<Grid.Column width={4}>
+								<Button
+									name="+"
+									onClick={() => this.addNum('+')}
+									fluid
+								>
+									{'+'}
+								</Button>
+							</Grid.Column>
+						</Grid.Row>
 
-					{/* ROW 7 */}
-					<Grid.Row>
-						<Grid.Column width={4}>
-							<Button
-								name="0"
-								onClick={() => this.addNum('0')}
-								fluid
-							>
-								{'0'}
-							</Button>
-						</Grid.Column>
-						<Grid.Column width={2}>
-							<Button
-								name="."
-								onClick={() => this.addNum('.')}
-								fluid
-							>
-								{'.'}
-							</Button>
-						</Grid.Column>
-						<Grid.Column width={2}>
-							<Button
-								name="="
-								onClick={this.calculate}
-								fluid
-							>
-								{'='}
-							</Button>
-						</Grid.Column>
-					</Grid.Row>
-				</Grid>
+						{/* ROW 7 */}
+						<Grid.Row>
+							<Grid.Column width={8}>
+								<Button
+									name="0"
+									onClick={() => this.addNum('0')}
+									fluid
+								>
+									{'0'}
+								</Button>
+							</Grid.Column>
+							<Grid.Column width={4}>
+								<Button
+									name="."
+									onClick={() => this.addNum('.')}
+									fluid
+								>
+									{'.'}
+								</Button>
+							</Grid.Column>
+							<Grid.Column width={4}>
+								<Button
+									name="="
+									onClick={this.calculate}
+									fluid
+								>
+									{'='}
+								</Button>
+							</Grid.Column>
+						</Grid.Row>
+					</Grid>
 			</Container>
 		)
 	}
