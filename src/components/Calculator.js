2854315e008f40b1a7a79699b4a5af993e1a746d
MrTj458 <mrtj458@gmail.com>
2018-10-27 11:26:24 -0600

    Initial setup

---
+++ src/components/Calculator.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {
+	Header,
+} from 'semantic-ui-react'
+
+class Calculator extends React.Component {
+	render() {
+		return (
+			<Header as="h1" textAlign="center">Calculator</Header>
+		)
+	}
+}
+
+export default Calculator
